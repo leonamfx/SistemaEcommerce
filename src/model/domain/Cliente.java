92a21c30289491f77cda4f17bfc2f627a587f679
Leonam Lima <leonamfx@gmail.com>
2019-03-09 00:25:45 -0300

    Criação da Classe Cliente.Java

---
+++ src/model/domain/Cliente.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.domain;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author leona
+ */
+public class Cliente {
+    
+   private Integer codigo;
+   
+   private String nome;
+    
+   public Cliente() {
+       
+   }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + Objects.hashCode(this.codigo);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (!Objects.equals(this.codigo, other.codigo)) {
+            return false;
+        }
+        return true;
+    }
+   
+}
